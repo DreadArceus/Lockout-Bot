eda138ead3dc2136508969fb394e8555154b46b8
dreadarceus <dreadarceus@gmail.com>
2021-06-25 13:30:09 +0530

    fix3p2

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ async def on_ready():
     global logging_channel
     logging_channel = await client.fetch_channel(os.environ.get("LOGGING_CHANNEL"))
     await logging_channel.send(f"Bot ready")
-    print("still here")
-    # dbconn.DbConn().erase_queue()
+    dbconn.DbConn().erase_queue()
 
     scheduler = AsyncIOScheduler()
     scheduler.add_job(update, 'interval', seconds=AUTO_UPDATE_TIME)
@@ -41,9 +40,10 @@ async def on_ready():
 
 
 async def update():
-    await tasks.update_matches(client)
-    await tasks.update_rounds(client)
-    await tasks.update_solos(client)
+    return
+    # await tasks.update_matches(client)
+    # await tasks.update_rounds(client)
+    # await tasks.update_solos(client)
 
 
 @client.event
